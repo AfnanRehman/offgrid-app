7aeedc1bb00b0afb7f52adaf79dfabbe1f1adc79
Afnan Rehman <afnan.rehman@gmail.com>
2024-08-27 23:18:33 -0400

    Initial Commit

---
+++ OffGrid/OffGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  OffGridApp.swift
+//  OffGrid
+//
+//  Created by Afnan Rehman on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct OffGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
